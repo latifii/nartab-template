56c9dfa2f3d8e6b5c2b773019a83be3abdb73098
latifii <latifii68@gmail.com>
2023-01-16 14:13:20 +0330

    test

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
-const menuIcon = document.querySelector(".main-menu__icon");
-const menuWrapper = document.querySelector(".menu-wrapper");
-const backdrop = document.querySelector(".backdrop");
-const closeIcon = document.querySelector(".close-icon");
-const contentBoxes = document.querySelectorAll(".content-head__box");
+const menuIcon = document.querySelector('.main-menu__icon')
+const menuWrapper = document.querySelector('.menu-wrapper')
+const backdrop = document.querySelector('.backdrop')
+const closeIcon = document.querySelector('.close-icon')
+const contentBoxes = document.querySelectorAll('.content-head__box')
 const serviceBoxes = document.querySelectorAll(
-  ".content-description .service-box"
-);
-const contentImgs = document.querySelectorAll(".content-screen");
-let index = 0;
+  '.content-description .service-box'
+)
+const contentImgs = document.querySelectorAll('.content-screen')
+let index = 0
 
-menuIcon.addEventListener("click", () => {
-  menuWrapper.classList.add("open");
-  backdrop.style.display = "block";
-});
-closeIcon.addEventListener("click", () => {
-  menuWrapper.classList.remove("open");
-  backdrop.style.display = "none";
-});
+menuIcon.addEventListener('click', () => {
+  menuWrapper.classList.add('open')
+  backdrop.style.display = 'block'
+})
+closeIcon.addEventListener('click', () => {
+  menuWrapper.classList.remove('open')
+  backdrop.style.display = 'none'
+})
 
 contentBoxes.forEach((box) =>
-  box.addEventListener("click", (e) => {
-    contentBoxes.forEach((box) => box.classList.remove("open"));
-    box.classList.add("open");
+  box.addEventListener('click', (e) => {
+    contentBoxes.forEach((box) => box.classList.remove('open'))
+    box.classList.add('open')
 
-    index = Array.prototype.indexOf.call(contentBoxes, e.currentTarget);
-    serviceBoxes.forEach((box) => box.classList.remove("open"));
-    serviceBoxes[index].classList.add("open");
+    index = Array.prototype.indexOf.call(contentBoxes, e.currentTarget)
+    serviceBoxes.forEach((box) => box.classList.remove('open'))
+    serviceBoxes[index].classList.add('open')
 
-    contentImgs.forEach((img) => img.classList.add("dn"));
-    contentImgs[index].classList.remove("dn");
+    contentImgs.forEach((img) => img.classList.add('dn'))
+    contentImgs[index].classList.remove('dn')
   })
-);
+)
 
-
-var swiper = new Swiper(".products", {
-
+var swiper = new Swiper('.products', {
   spaceBetween: 10,
-  loop:true,
+  loop: true,
   centeredSlides: true,
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   breakpoints: {
     0: {
@@ -52,7 +50,7 @@ var swiper = new Swiper(".products", {
     },
     550: {
       slidesPerView: 2,
-      spaceBetween: 40
+      spaceBetween: 40,
     },
     768: {
       slidesPerView: 3,
@@ -61,6 +59,4 @@ var swiper = new Swiper(".products", {
       slidesPerView: 4,
     },
   },
-});
-
-
+})
